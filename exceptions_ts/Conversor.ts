44f08505f575c3395b4cffbb26281acb5a40c052
caaarolb <carolinelboaventura@gmail.com>
2024-12-16 11:54:24 -0300

    Exceptions

---
+++ exceptions_ts/Conversor.ts
@@ -0,0 +1,17 @@
+﻿
+try{
+    const resultado = converte('abc');
+
+console.log(resultado);
+} catch(error: any){
+    if (error instanceof TypeError)
+        console.error("\n Você digitou um número ao inés de uma string")
+    else
+    console.log('Erro inesperado!', error.message)
+} finally{
+    console.log("\nPrograma finalizado com sucesso!")
+}
+
+export function converte(conteudo: any): string {
+    return conteudo.toUpperCase();
+}
---
+++ transporte/TestaTransporte.ts
@@ -0,0 +1,23 @@
+import { Automovel } from "./Automovel";
+import { Terrestre } from "./Terrestre";
+import { Transporte } from "./Transporte";
+
+function main(){
+
+    const t1 =  new Transporte(5);
+    t1.visualizar();
+
+    const tr1 = new Terrestre(5, 4 ,100);
+    tr1.visualizar();
+
+    const a1 = new Automovel(5, 4, 250, "Verde", 5, "FLV-4566", 5);
+    a1.visualizar();
+
+     /* get e set do jeito moderno
+    a1.cor = "Laranja";
+    console.log(a1.cor);*/
+
+      
+}
+
+main();
